90fdd0f02697b78e9a420beab2040d7364f5c215
d2217bb <daniela.boros@isa.utm.md>
2025-05-20 10:34:28 +0300

    commit

--- app/src/main/java/com/danielaboros/appandroid/ui/theme/Theme.kt
+++ app/src/main/java/com/danielaboros/appandroid/ui/theme/Theme.kt
@@ -1,2 +1,32 @@
 package com.danielaboros.appandroid.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+
+private val LightColors = lightColorScheme(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
+)
+
+private val DarkColors = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
+)
+
+@Composable
+fun AndroidAppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) DarkColors else LightColors
+
+    MaterialTheme(
+        colorScheme = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
--- app/src/main/java/com/danielaboros/appandroid/ui/theme/Type.kt
+++ app/src/main/java/com/danielaboros/appandroid/ui/theme/Type.kt
@@ -1,2 +1,12 @@
 package com.danielaboros.appandroid.ui.theme
 
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
+
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontSize = 16.sp,
+        lineHeight = 24.sp
+    )
+)
